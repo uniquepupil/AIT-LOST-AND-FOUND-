7558981aa7b0df07ee6013b394550e26c05e6cef
Parikshit <prikshitbhardwaj6@gmail.com>
2023-12-05 20:44:21 +0530

    Update lost.js

--- forms/lost.js
+++ forms/lost.js
@@ -1,16 +1,18 @@
-document.getElementById('lostItemForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-    
-    // Get form values
-    const itemName = document.getElementById('itemName').value;
-    const description = document.getElementById('description').value;
-    const contact = document.getElementById('contact').value;
-    
-    // Display confirmation message
-    const confirmation = document.getElementById('confirmation');
-    confirmation.innerHTML = `Thank you for submitting! Your ${itemName} has been reported lost. We'll contact you at ${contact} if found.`;
-    
-    // Clear form fields
-    document.getElementById('lostItemForm').reset();
-  });
-  
+// Function to increase input size while typing
+document.addEventListener('input', function (event) {
+  if (event.target.tagName.toLowerCase() === 'input' || event.target.tagName.toLowerCase() === 'textarea') {
+    event.target.style.width = 'calc(' + (event.target.value.length + 1) + 'ch)';
+  }
+});
+
+// Function to handle form submission
+document.getElementById('foundItemForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Display confirmation message
+  const confirmation = document.getElementById('foundConfirmation');
+  confirmation.innerHTML = `<p>Thank you for reporting the found item!<br>We'll reach out if there's a potential match.</p>`;
+
+  // Clear form fields after submission
+  document.getElementById('foundItemForm').reset();
+});
